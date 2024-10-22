3d060bfe02bb30fe6c58059084853b52a54889a5
tumlinson@stsci.edu <tumlinson@stsci.edu>
2024-10-22 11:49:10 -0400

    refactor directories and move JSON, setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name="hwo_sci_eng",
+      version="0.1",
+      description="HWO Science Engineering Interface", 
+      author="Breann Sitarski, NASA/GSFC, Jason Tumlinson, STScI", 
+      author_email=" ",
+      license="CC",
+      keywords=["simulation", "astronomy", "astrophysics"],
+      url="https://github.com/HWO-GOMAP-Working-Groups/Sci-Eng-Interface", 
+      packages=["obs_config", "json", "utils"], 
+      package_data={'':['*.yaml', '*.json', 'json/*json', 'obs_config/*.yaml']}, 
+      include_package_data=True,
+      classifiers=[
+          "Programming Language :: Python :: 3.6",
+      ],
+      install_requires=[
+            "setuptools>=61.0", 
+            "yaml", 
+      ],
+)
