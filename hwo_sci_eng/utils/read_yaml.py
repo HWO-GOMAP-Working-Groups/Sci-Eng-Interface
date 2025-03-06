18287214889fee139f3886316b8e715f5422fc5b
Adric Riedel <adric.riedel@gmail.com>
2025-03-06 13:29:22 -0500

    Replace the individual loaders with a simpler parameterized one

--- hwo_sci_eng/utils/read_yaml.py
+++ hwo_sci_eng/utils/read_yaml.py
@@ -1,44 +1,19 @@
 import os, yaml
 
-def eac1(): 
-    
-    with open(os.getenv('SCI_ENG_DIR') + '/obs_config/Tel/EAC1.yaml', 'r') as f:
-        eac1 = yaml.load(f, Loader=yaml.SafeLoader)
+paths = {
+    "eac1": "/obs_config/Tel/EAC1.yaml",
+    "eac2": "/obs_config/Tel/EAC2_draft.yaml",
+    "eac3": "/obs_config/Tel/EAC2_draft.yaml",
+    "hri": "/obs_config/HRI/HRI.yaml",
+    "uvi": "/obs_config/UVI/UVI.yaml",
+    "ci": "/obs_config/CI/CI.yaml"
+}
+
+def read(component):
+    """
+    A generic component reader
+    """
+    with open(os.getenv('SCI_ENG_DIR') + paths[component.lower()], 'r') as f:
+        comp_yaml = yaml.load(f, Loader=yaml.SafeLoader)
    
-    return eac1 
-
-def eac2(): 
-    
-    with open(os.getenv('SCI_ENG_DIR') + '/obs_config/Tel/EAC2_draft.yaml', 'r') as f:
-        eac2 = yaml.load(f, Loader=yaml.SafeLoader)
-
-    return eac2 
-
-def eac3(): 
-    
-    with open(os.getenv('SCI_ENG_DIR') + '/obs_config/Tel/EAC3_draft.yaml', 'r') as f:
-        eac3 = yaml.load(f, Loader=yaml.SafeLoader)
-
-    return eac3 
-
-def hri(): 
-    
-    with open(os.getenv('SCI_ENG_DIR') + '/obs_config/HRI/HRI.yaml', 'r') as f:
-        hri = yaml.load(f, Loader=yaml.SafeLoader)
-   
-    return hri 
-
-def uvi(): 
-
-    with open(os.getenv('SCI_ENG_DIR') + '/obs_config/UVI/UVI.yaml', 'r') as f:
-        uvi= yaml.load(f, Loader=yaml.SafeLoader)
-
-    return uvi 
-
-def ci(): 
-
-    with open(os.getenv('SCI_ENG_DIR') + '/obs_config/CI/CI.yaml', 'r') as f:
-        ci = yaml.load(f, Loader=yaml.SafeLoader)
-
-    return ci 
-
+    return comp_yaml
